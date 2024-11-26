e1b77418aa776d9c7dc678b33d9f2fe6ba416494
VictorCottar <victorcottarms@outlook.com>
2024-11-25 23:34:14 -0300

    Adicionar e remover participante da lista

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import { Text, View, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons'
 
@@ -5,21 +6,24 @@ import { styles } from './styles'
 import { Participant } from '@/components/Participant'
 
 export function Home() {
-  const participants = ['Victor Cottar', 'Diego', 'Rafael', 'Pedro', 'Gustavo', 'Lucas', 'João', 'Maria', 'José', 'Ana', 'Afonso', 'Joaquim', 'Fernando', 'Mariana', 'Carla', 'Juliana', 'Marta', 'Cristina', 'Sofia', 'Miguel', 'Ricardo',]
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState('');
 
   function handleParticipantAdd() {
 
-    if (participants.includes('Victor Cottar')) {
+    if (participants.includes(participantName)) {
       return Alert.alert('Participante', 'Já existe um participante na lista com esse nome.')
     }
 
+    setParticipants(prevState => [...prevState, participantName]);
+    setParticipantName('');
   }
 
   function handleParticipantRemove(name: string) {
     Alert.alert('Remover', `Deseja remover o participante ${name}?`, [
       {
         text: 'Sim',
-        onPress: () => Alert.alert('Deletado!')
+        onPress: () => setParticipants(prevState => prevState.filter(participant => participant !== name))
       },
       {
         text: 'Não',
@@ -39,6 +43,8 @@ export function Home() {
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor='#6B6B6B'
+          onChangeText={setParticipantName}
+          value={participantName}
         />
 
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
